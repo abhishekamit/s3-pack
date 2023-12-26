63d6d96324dcff480a0ba48f9f7a52a8be9522de
Abhishek Amit <abhishekamit@gmail.com>
2023-12-26 13:38:12 -0400

    Initial commit

--- pack.ts
+++ pack.ts
@@ -1,41 +1,139 @@
-// This import statement gives you access to all parts of the Coda Packs SDK.
 import * as coda from "@codahq/packs-sdk";
 
-// This line creates your new Pack.
 export const pack = coda.newPack();
 
-// Here, we add a new formula to this Pack.
-pack.addFormula({
-  // This is the name that will be called in the formula builder.
-  // Remember, your formula name cannot have spaces in it.
-  name: "Hello",
-  description: "A Hello World example.",
+pack.setUserAuthentication({
+  type: coda.AuthenticationType.AWSAccessKey,
+  service: "s3",
+});
 
-  // If your formula requires one or more inputs, you’ll define them here.
-  // Here, we're creating a string input called “name”.
+pack.addNetworkDomain("amazonaws.com");
+
+function getBaseUrl(region: string, bucket: string) {
+  return `https://${bucket}.s3.${region}.amazonaws.com/`
+}
+
+pack.addFormula({
+  name: "CreateBucket",
+  description: "Creates a bucket.",
   parameters: [
     coda.makeParameter({
       type: coda.ParameterType.String,
-      name: "name",
-      description: "The name you would like to say hello to.",
+      name: "bucket",
+      description: "The name of the bucket to create.",
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "region",
+      description: "Region to create the bucket in (i.e. us-east-1).",
     }),
   ],
-
-  // The resultType defines what will be returned in your Coda doc. Here, we're
-  // returning a simple text string.
   resultType: coda.ValueType.String,
-
-  // These examples are shown to the user, to help them understand how to use
-  // the formula.
-  examples: [
-    { params: ["World"], result: "Hello World!" },
-    { params: ["Coda"], result: "Hello Coda!" },
-  ],
-
-  // Everything inside this execute statement will happen anytime your Coda
-  // formula is called in a doc. An array of all user inputs is always the 1st
-  // parameter.
-  execute: async function ([name], context) {
-    return "Hello " + name + "!";
+  execute: async function (args, context) {
+    const [bucket, region] = args;
+    await context.fetcher.fetch({
+      method: "PUT",
+      url: getBaseUrl(region, bucket),
+    });
+    return "";
   },
+  isAction: true,
+});
+
+const ObjectSchema = coda.makeObjectSchema({
+  properties: {
+    key: { type: coda.ValueType.String },
+    lastModified: { type: coda.ValueType.String, codaType: coda.ValueHintType.DateTime },
+    etag: { type: coda.ValueType.String },
+    size: { type: coda.ValueType.Number },
+    storageClass: { type: coda.ValueType.String },
+    bucket: { type: coda.ValueType.String },
+    region: { type: coda.ValueType.String },
+  },
+  displayProperty: "key",
+  idProperty: "key",
+});
+
+pack.addSyncTable({
+  name: "Objects",
+  description: "List objects in an S3 bucket.",
+  identityName: "S3",
+  schema: ObjectSchema,
+  formula: {
+    name: "Objects",
+    description: "List objects in an S3 bucket.",
+    parameters: [
+      coda.makeParameter({
+        type: coda.ParameterType.String,
+        name: "bucket",
+        description: "Bucket to list.",
+      }),
+      coda.makeParameter({
+        type: coda.ParameterType.String,
+        name: "region",
+        description: "Region the bucket is in (i.e. us-east-1).",
+      }),
+    ],
+    execute: async function (args, context) {
+      const [bucket, region] = args;
+      const response = await context.fetcher.fetch({
+        method: "GET",
+        url: coda.withQueryParams(getBaseUrl(region, bucket), {
+          'list-type': '2',
+          'continuation-token': context.sync.continuation?.continuationToken,
+        }),
+      });
+      const data = response.body;
+      const result = data.Contents.map((item) => {
+        return {
+          ...item,
+          etag: item.ETag[0].replace(/"/g, ""),
+          bucket,
+          region,
+        }
+      });
+      return {
+        result,
+        continuation: data.IsTruncated ? { continuationToken: data.NextContinuationToken } : undefined,
+      };
+    },
+  },
+});
+
+pack.addFormula({
+  name: "PutObject",
+  description: "Adds an object to a bucket.",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "bucket",
+      description: "Bucket to add object to.",
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "region",
+      description: "Region the bucket is in (i.e. us-east-1).",
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "key",
+      description: "Object key for which the PUT action was initiated.",
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "contents",
+      description: "Contents of object.",
+    }),
+  ],
+  resultType: coda.ValueType.String,
+  execute: async function (args, context) {
+    const [bucket, region, key, contents] = args;
+    await context.fetcher.fetch({
+      method: "PUT",
+      url: `${getBaseUrl(region, bucket)}/${key}`,
+      body: contents,
+    });
+    return '';
+  },
+  isAction: true,
 });
